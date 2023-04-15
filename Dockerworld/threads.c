e06cbc44bb611c44fc069946d580f81bcf42d8d5
Mario Enrique Pisquiy Gómez <mariopisquiy@ufm.edu>
2023-04-14 19:09:48 -0600

    Thread funcional
    Primer thread funcional del proyecto

--- Dockerworld/threads.c
+++ Dockerworld/threads.c
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
         fprintf (stderr, "uso: a.out <valor entero>\n"); 
         return -1;
     }
-    if (atoi(argv[1]) < 0) {
-        fprintf (stderr, "%d debe ser >= 0\n", atoi(argv[1] )) ; 
+    if (atoi(argv[1])<0){
+        fprintf(stderr, "%d debe ser >= 0 \n", atoi(argv[1]));
         return -1;
     }
     /* obtener los atributos predeterminados */ 
