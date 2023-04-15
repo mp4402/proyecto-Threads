586533277d8065f7ab9237c7e3394197a0237ae7
Mario Enrique Pisquiy Gómez <mariopisquiy@ufm.edu>
2023-04-14 18:55:59 -0600

    Thread prueba 1
    Primera prueba para ejecutar un código multi thread en C en Docker

---
+++ Dockerworld/threads.c
@@ -0,0 +1,37 @@
+#include <pthread.h>
+#include <stdio.h>
+
+int sum; /*las hebras comparten esta variable*/ 
+void *runner(void *param); /* la hebra */
+
+int main(int argc, char *argv[])
+{
+    pthread_t tid; /* el identificador de la hebra */
+    pthread_attr_t attr; /* conjunto de atributos de la hebra */
+    if (argc != 2) {
+        fprintf (stderr, "uso: a.out <valor entero>\n"); 
+        return -1;
+    }
+    if (atoi(argv[1]) < 0) {
+        fprintf (stderr, "%d debe ser >= 0\n", atoi(argv[1] )) ; 
+        return -1;
+    }
+    /* obtener los atributos predeterminados */ 
+    pthread_attr_init(&attr);
+    /* crear la hebra */
+    pthread_create(&tid, &attr, runner, argv[1]); 
+    /* esperar a que la hebra termine */ 
+    pthread_join(tid,NULL);
+
+    printf("sum = %d\n", sum);
+}
+/* La hebra inicia su ejecución en esta función */ 
+void *runner(void *param)
+{
+    int i, upper = atoi(param);
+    sum = 0;
+    for (i = 1; i <= upper; i++){
+        sum += i;
+    } 
+    pthread_exit(0);
+}
